b23054ca12e83a072c7e8c5e680e8f48ea92d1d5
Gabe Yamartino <gabeyamartino@gmail.com>
2022-11-04 15:34:17 -0400

    Product route setup with aggregation query working correctly. Styles query is mostly working.

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,4 @@
-const { Pool, Client } = require('pg');
+const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
@@ -11,8 +11,65 @@ const pool = new Pool({
 
 pool.connect();
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+const getAllProducts = () => {
+  return pool.query('SELECT * FROM product WHERE id < 100000')
+}
+
+
+
+//`SELECT * FROM product INNER JOIN features ON product.id = features.product_id AND product.id = ${itemId}`
+const getProduct = (req, res) => {
+  return pool.query(`SELECT * FROM (
+    SELECT *, (SELECT json_agg(f) FROM
+      (SELECT feature, value FROM features WHERE product_id = product.id) f ) as features
+      FROM product WHERE id = ${req.params.product_id}) p`)
+      .then((data) => {
+        res.send({ data: data.rows[0] });
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(500)
+        res.end();
+      })
+}
+
+const getStyles = (request, response) => {
+  var product_id = request.params.product_id;
+  var query = `SELECT * FROM styles WHERE product_id = ${product_id}`;
+  pool
+    .query(query)
+    .then(res => {
+      var styles = res.rows;
+      let photoPromises = [];
+      let skuPromises = [];
+      styles.forEach((style) => {
+        photoPromises.push(pool.query(`SELECT thumbnail_url, url FROM photos WHERE "style_id" = ${style.style_id}`)
+          .then(res => res.rows)
+        )
+        skuPromises.push(pool.query(`SELECT size, quantity FROM skus WHERE "style_id" = ${style.style_id}`)
+          .then(res => res.rows))
+        })
+        Promise.all(photoPromises)
+          .then((res) => {
+            styles.forEach((style, index) => {
+              style.photo = res[index];
+            })
+            Promise.all(skuPromises)
+              .then((res) => {
+                styles.forEach((style, index) => {
+                  style.skus = res[index];
+                })
+                response.send(styles);
+              })
+          })
+    })
+    .catch(err => {
+      console.error('Error executing to get style information', err.stack);
+      res.status(500);
+    });
+}
+
+module.exports.getAllProducts = getAllProducts;
+module.exports.getProduct = getProduct;
+module.exports.getStyles = getStyles;
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,58 @@
 const express = require('express');
-const app = express()
-const db = './db/index.js';
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+const db = require('./db/index.js');
+const {getAllProducts, getProduct, getStyles} = require('./db/index.js')
 
-app.listen(3000, () => {
-  console.log('listening on port 3000')
+//app.use(bodyParser.json())
+app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (req, res) => {
+
+  //res.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+//ALL PRODUCTS
+
+app.get('/products', (req, res) => {
+  console.log('products route setup!')
+  getAllProducts()
+  .then((data) => {
+    res.send(data)
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500)
+    res.end();
+  })
+})
+
+//SINGLE PRODUCT
+
+app.get('/products/:product_id', getProduct);
+
+// //PRODUCT STYLES
+
+app.get('/products/:product_id/styles', getStyles)
+// (req, res) => {
+//   getStyles(req.params.product_id)
+//   .then((data) => {
+//     console.log(data)
+//     res.send(data);
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//     res.status(500)
+//     res.end();
+//   })
+// })
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
 })
